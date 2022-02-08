f248809545cb1e6c30eec1f383b9f42258f5866c
arthurwinck <apwinck@gmail.com>
2022-02-08 20:23:54 -0300

    Bebe nasceu (prematuro)

---
+++ straights.lisp
@@ -0,0 +1,49 @@
+
+(defparameter board (make-array '(6 6) :initial-element 0))
+
+(defun possible (i j n board)
+    (+ 0 1)
+)
+
+;; (defun str8s-solver (board)
+;;     (iter (loop for i in '(1 2 3 4 5 6)
+;;         (iter (loop for j in '(1 2 3 4 5 6)
+;;             (if (= (aref board i j) 0)
+;;                 (loop for n in '(1 2 3 4 5 6))
+;;                     (if (= (possible i j n board) 1)
+;;                         (setf (aref board i j) n)))
+;;                 )
+;;             )      
+;;         )
+;;     )
+;; )
+
+
+
+;;                        (setf (aref board i j) 0)
+;; 
+
+
+
+(defun str8s-solver (board)
+    (loop for i in '(0 1 2 3 4 5)
+        do (loop for j in '(0 1 2 3 4 5)
+            do (if (= (aref board i j) 0)
+                (loop for n in '(0 1 2 3 4 5)
+                    do (if (= (possible i j n board) 1)
+                        (progn
+                            (setf (aref board i j) n)
+                        )
+                    )
+                )
+            )
+        )
+    )
+)
+
+
+(defun main()
+    (str8s-solver board)
+    (write (write-to-string board))
+)
+(main)
